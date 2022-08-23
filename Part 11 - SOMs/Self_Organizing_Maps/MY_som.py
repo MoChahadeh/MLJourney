004957247859dce1e3e5c350211878878711f86e
Mo Chahadeh <applednd2002@gmail.com>
2022-08-23 21:24:16 +0300

    SOMs

---
+++ Part 11 - SOMs/Self_Organizing_Maps/MY_som.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, 1:-1].values
+Y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom
+
+som = MiniSom(x = 10, y = 10, input_len = 14, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+
+from pylab import bone, pcolor, colorbar, plot, show
+
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markersize = 10,
+         markeredgewidth = 2)
+
+show()
